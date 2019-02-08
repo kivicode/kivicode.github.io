77dc7f1bb8458393ce09dbb0b47395ef593a39d4
kivicode <31818467+kivicode@users.noreply.github.com>
2019-02-08 09:46:46 +0300

    Add new ver

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+function setDefaults(){
+  document.getElementById("default").click()
+}
+
+function openCity(evt, cityName) {
+  console.log(evt);
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
