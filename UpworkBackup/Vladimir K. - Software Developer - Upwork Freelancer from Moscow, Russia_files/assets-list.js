46e7a019b0d904bb56f6d52cb2fae0097d38d4fc
kivicode <31818467+kivicode@users.noreply.github.com>
2019-03-11 21:02:04 +0300

    Update

--- UpworkBackup/Vladimir K. - Software Developer - Upwork Freelancer from Moscow, Russia_files/assets-list.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var fileList = ["\/ab\/assets\/Bpa\/9b18b11\/js\/bpa-ui-combined.js"];
-    for (var i = 0, l = fileList.length; i < l; i++ ) {
-        document.write('<script defer src="' + fileList[i] + '"></script>');
-    }
-})();
