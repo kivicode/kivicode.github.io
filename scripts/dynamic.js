e94dd71250851bd69945e2e169ba898a1847ee86
kivicode <31818467+kivicode@users.noreply.github.com>
2019-03-26 23:43:36 +0300

    Added alpha version of the github real time preloading

--- scripts/dynamic.js
+++ scripts/dynamic.js
@@ -15,6 +15,82 @@ function loadTags() {
     tags = d;
   });
 }
+let test_l;
+
+let repos, lng, icon;
+
+let projects_test = []
+
+function generateGitHubProjects() {
+  $.when(getRepos()).done(function(a) {
+    for (r in repos) {
+      let project = {}
+      let repo = repos[r]
+      $.when(getLangs(repo.name)).done(function(b) {
+        if (Object.keys(lng).length > 0) {
+          let url = "https://raw.githubusercontent.com/kivicode/" + "AndroidDetector" + "/master/icon.png"
+          project["img"] = url
+          project["name"] = repo.name;
+          let langs = [];
+          // let sliced = lng.slice(0, Math.min(4, Object.keys(lng).length));
+          for (var i of Object.keys(lng).slice(0, Math.min(5, Object.keys(lng).length)).map(entry => entry)) {
+            let freq = lng[i]
+            let name = i
+            langs.push([freq, name])
+          }
+          projects_test.push(project)
+        }
+      })
+    }
+    return projects_test;
+  })
+}
+
+function getRepos() {
+  return $.ajax({
+    url: "https://api.github.com/users/kivicode/repos",
+    dataType: "json",
+  }).done(function(rps) {
+    repos = rps
+    for (r in repos) {
+      let project = {}
+      let repo = repos[r]
+      $.when(getLangs(repo.name)).done(function(b) {
+        if (Object.keys(lng).length > 0) {
+          let url = "https://raw.githubusercontent.com/kivicode/" + "AndroidDetector" + "/master/icon.png"
+          project["img"] = url
+          project["name"] = repo.name;
+          let langs = [];
+          // let sliced = lng.slice(0, Math.min(4, Object.keys(lng).length));
+          for (var i of Object.keys(lng).slice(0, Math.min(5, Object.keys(lng).length)).map(entry => entry)) {
+            let freq = lng[i]
+            let name = i
+            langs.push([freq, name])
+          }
+          projects_test.push(project)
+        }
+      })
+    }
+  });
+}
+
+function getGitIcon(repo) {
+  return $.ajax({
+    url: "https://raw.githubusercontent.com/kivicode/" + "AndroidDetector" + "/master/icon.png"
+  }).done(function(icn) {
+    console.log(icn);
+    icon = icn
+  });
+}
+
+function getLangs(name) {
+  return $.ajax({
+    url: "https://api.github.com/repos/kivicode/" + name + "/languages",
+    dataType: "json",
+  }).done(function(l) {
+    lng = l
+  });
+}
 
 function loadColors() {
   return $.ajax({
@@ -47,7 +123,7 @@ function getFilteredProjects() {
 }
 
 function render() {
-  $.when(loadTags(), loadColors(), loadProjects()).done(function(a1, a2, a3) {
+  $.when(loadTags(), loadColors()).done(function() {
     tags_app = new Vue({
       el: '#tags',
       data: {
@@ -55,11 +131,14 @@ function render() {
       }
     });
 
-    projects_app = new Vue({
-      el: '#content-field',
-      data: {
-        projects: getFilteredProjects().length == 0 ? projects : getFilteredProjects()
-      }
+    $.when(getRepos()).done(function() {
+      projects = projects_test;
+      projects_app = new Vue({
+        el: '#content-field',
+        data: {
+          projects: getFilteredProjects().length == 0 ? projects : getFilteredProjects()
+        }
+      });
     });
   });
 }
@@ -69,7 +148,7 @@ function updateFilter(id) {
   if (id != "") {
     let tag = document.getElementById(id);
     if (filters.indexOf(id.toLowerCase()) == -1) {
-      tag.style.cssText = "background-color: rgb(255, 236, 130);";
+      tag.style.cssText = "background-color: orange;";
       filters.push(id.toLowerCase())
     } else {
       tag.style.cssText = ""
@@ -84,12 +163,8 @@ function updateFilter(id) {
   if (filt.length == 0 && filters.length > 0) {
     projects_app.projects = [];
   } else if (filt.length == 0 && filters.length == 0) {
-    projects_app.projects = projects;;
+    projects_app.projects = projects;
   }
-  // if (filters.length == 0) {
-  //   projects_app.projects = [];
-  // }
-
 }
 
 
@@ -98,11 +173,15 @@ function getColor(v) {
 }
 
 function getImg(v) {
-  return "../images/projects/" + v.project.img;
+  return v.project.img;
 }
 
 function getName(v) {
   return v.tag.name;
+}
+
+function getRepoName(v) {
+  return v._props.project.name.match(/[A-Z, _][a-z]+/g).join(' ');
 }
 
 function generate() {
@@ -135,13 +214,25 @@ function generate() {
         <img :src="src" />
       </div>
       <div class="preview-title">
-        <a> {{ project.name }}</a>
+        <a class="button" :href="hrefname">{{ project.name }}</a>
+      </div>
+
+      <div :id="project.name" class="overlay">
+      	<div class="popup">
+      		<h3>{{ name }}</h3>
+      		<a class="close" href="#content-field">×</a>
+      		<div class="content">
+      			Thank to pop me out of that button, but now i'm done so you can close this window.
+      		</div>
+      	</div>
       </div>
     </div>
     `,
     data() {
       return {
-        src: getImg(this)
+        src: getImg(this),
+        name: getRepoName(this),
+        hrefname: '#' + this._props.project.name
       }
     },
     props: {
